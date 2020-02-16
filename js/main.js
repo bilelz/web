6c5b46eb3b2e203accb40482ebfd4b6ac17cf52a
bilelz <bilelz@gmail.com>
2020-02-16 19:22:24 +0100

    scroll smooth

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
 
+  
+  (function () {
+    'use strict';
+
+    var blog = fetch("https://blog.bilelz.fr/feeds/posts/default?alt=json");
+    blog.then(function (response) {
+        console.log('response', response);
+        console.log('header', response.headers.get('Content-Type'));
+        return response.json();
+    }).then(function (json) {
+        console.log('got text', json);
+        
+    }).catch(function (ex) {
+        console.log('failed', ex);
+    });
+})();
